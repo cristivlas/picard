f4fd8fdc992ffbf341ed31751b67258d4bf17855
Cristian Vlasceanu <cvlasceanu@tableau.com>
2020-03-09 19:13:54 -0700

    replace backslash with fwd slash in resource URLs

--- cache.py
+++ cache.py
@@ -8,23 +8,30 @@ import zipfile
 from PIL import Image, ImageFont
 
 DataPath = ''
+
+def join(elems):
+    return str('/').join(elems)
+
+def normpath(path):
+    return join(os.path.normpath(path).split(os.path.sep))
+
 class CacheFile:
-    CurrentWorkDir = os.path.splitdrive(os.getcwd())[1]
-    Root = os.path.join(CurrentWorkDir, '.cache')
+    CurrentWorkDir = normpath(os.path.splitdrive(os.getcwd())[1])
+    Root = join((CurrentWorkDir, '.cache'))
     Verbose = sys.modules['__main__'].verbose
 
     def __init__(self, url):
         parsedUrl = portable.urlparse(url)
         start = 1 if os.path.isabs(parsedUrl.path) else 0
-        path = os.path.sep.join(parsedUrl.path.split('/')[start:])
+        path = join(parsedUrl.path.split('/')[start:])
         if not os.path.splitext(path)[1]:
             path = self.resolvePath(parsedUrl, path)
         if len(parsedUrl.netloc):
-            path = os.path.join(os.path.join(CacheImage.Root, parsedUrl.netloc), path)
+            path = join((CacheImage.Root, parsedUrl.netloc, path))
         else:
             if not os.path.isabs(path):
-                url = os.path.normpath(os.path.sep.join([DataPath, path]))
-            path = os.path.join(CacheImage.Root, path)
+                url = join((DataPath, path))
+            path = join((CacheImage.Root, path))
 
         path = CacheFile.getValidPathName(path)
         portable.makedirs(os.path.split(path)[0])
@@ -32,7 +39,7 @@ class CacheFile:
         if not os.path.exists(path):
             if not parsedUrl.scheme:
                 if not os.path.isabs(url):
-                    url = str('/').join([CacheFile.CurrentWorkDir, url])
+                    url = join([CacheFile.CurrentWorkDir, url])
                 url = "file://" + url
             print ('Downloading', url, '...')
             portable.urlretrieve(url, path)
@@ -72,7 +79,7 @@ class CacheImage(CacheFile):
             print (e)
 
     def resolvePath(self, url, path):
-        return os.path.join(path, 'image.jpg')
+        return join((path, 'image.jpg'))
 
 class CacheFont(CacheFile):
     Cache = {}
@@ -98,4 +105,4 @@ class CacheFont(CacheFile):
     def resolvePath(self, url, path):
         if url.query and url.netloc=='dl.dafont.com':
             return url.query.split('=')[1] + '.zip'
-        return os.path.join(path, 'font.zip')
+        return join((path, 'font.zip'))
